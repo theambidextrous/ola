b8468a4f582af39a1a28b8ff92c6964fd700e56c
idd juma otuya <jotuya@HOSOFTWARE02.familyfinance.co.ke>
2019-11-12 15:36:57 +0300

    mongo db url change

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 // //staging
 // const mongoDB = 'mongodb://127.0.0.1/olapi';
 // console.log(mongoDB);
-var username = process.env.MONGO_DB_USERNAME;
-var password = process.env.MONGO_DB_PASSWORD;
+var username = process.env.MONGODB_USER;//MONGO_DB_USERNAME;
+var password = process.env.MONGODB_PASSWORD;//MONGO_DB_PASSWORD;
 var host = process.env.MONGODB_SERVICE_HOST;
 var port = process.env.MONGODB_SERVICE_PORT;
-var database = process.env.MONGO_DB_DATABASE;
+var database = process.env.MONGODB_DATABASE;//MONGO_DB_DATABASE;
 var mongoDB = 'mongodb://' + username + ':' + password +'@' + host + ':' + port + '/' + database;
 console.log(mongoDB);
 mongoose.connect(mongoDB, {
