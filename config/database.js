b1b86830c0fd1b2925bef6db57fdbed92e048633
idd juma otuya <jotuya@HOSOFTWARE02.familyfinance.co.ke>
2019-11-12 13:36:25 +0300

    mongo db url change

--- config/database.js
+++ config/database.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 //staging
 // const mongoDB = 'mongodb://localhost/ola';
 //production - openshift server
+console.log(util.inspect(process.env)) 
+
 const mongoDB = process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME;
 // if (process.env.OPENSHIFT_MONGODB_DB_URL) {
 //     mongoDB = process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME;
