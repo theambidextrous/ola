db3641b702d02097928d30fde065820654f36e29
idd juma otuya <jotuya@HOSOFTWARE02.familyfinance.co.ke>
2019-11-13 11:24:53 +0300

    mongo connection error handling

--- config/database.js
+++ config/database.js
@@ -6,12 +6,12 @@ var password = process.env.MONGODB_PASSWORD || 'openlocationsapi';
 var host = process.env.MONGODB_SERVICE_HOST || '127.0.0.1';
 var port = process.env.MONGODB_SERVICE_PORT || 27017;
 var database = process.env.MONGODB_DATABASE || 'openlocationsapidb';
-var mongoDB = 'mongodb://' + username + ':' + password +'@' + host + ':' + port + '/' + database;
+var mongoDB = 'mongodb://' + username + ':' + password +'@' + host + ':' + port + '/' + database + '?authSource=admin';
 var url = 'mongodb://127.0.0.1:27017/openlocationsapidb?authSource=admin';
 console.log(mongoDB);
 console.log(url);
 
-mongoose.connect(url).then(() => {
+mongoose.connect(mongoDB).then(() => {
 console.log("Connected to Database");
 }).catch((err) => {
     console.log("Not Connected to Database, ERROR! ", err);
