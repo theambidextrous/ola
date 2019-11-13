767dd4437e6d7e7004c07291a21d68f9c2b6aed0
idd juma otuya <jotuya@HOSOFTWARE02.familyfinance.co.ke>
2019-11-13 12:12:03 +0300

    mongo connection error handling

--- config/database.js
+++ config/database.js
@@ -7,12 +7,19 @@ var host = process.env.MONGODB_SERVICE_HOST || '172.30.3.175';
 var port = process.env.MONGODB_SERVICE_PORT || 27017;
 var database = process.env.MONGODB_DATABASE || 'openlocationsapidb';
 var mongoDB = 'mongodb://' + username + ':' + password +'@' + host + ':' + port + '/' + database + '?authSource=admin';
-console.log(mongoDB);
-mongoose.connect(mongoDB).then(() => {
-console.log("Connected to Database");
+//console.log(mongoDB);
+mongoose.connect('mongodb://openlocationsapimongodb:27017/openlocationsapidb', {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('Connected to mongo service...');
 }).catch((err) => {
-    console.log("Not Connected to Database, ERROR! ", err);
+    console.log('Error connecting.... : ', err);
 });
+// mongoose.connect(mongoDB).then(() => {
+// console.log("Connected to Database");
+// }).catch((err) => {
+//     console.log("Not Connected to Database, ERROR! ", err);
+// });
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
 
