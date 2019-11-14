01640a5a912571a9d49518f830a56d4486980846
idd juma otuya <jotuya@HOSOFTWARE02.familyfinance.co.ke>
2019-11-14 13:10:14 +0300

    db config

--- config/database.js
+++ config/database.js
@@ -1,12 +1,4 @@
 const mongoose = require('mongoose');
-var util = require('util');
-// console.log(util.inspect(process.env));
-var username = process.env.MONGODB_USER || 'openlocationsapi';
-var password = process.env.MONGODB_PASSWORD || 'openlocationsapi';
-var host = process.env.MONGODB_SERVICE_HOST || '172.30.3.175';
-var port = process.env.MONGODB_SERVICE_PORT || 27017;
-var database = process.env.MONGODB_DATABASE || 'openlocationsapidb';
-//console.log(mongoDB);
 mongoose.connect('mongodb://openlocationsapi:openlocationsapi@openlocationsapimongodb:27017/openlocationsapidb', {
     useNewUrlParser: true
 }).then(() => {
